c177c7d3c74d415c2ed5e647c64e889ab7892482
Axkita01 <94666562+Axkita01@users.noreply.github.com>
2022-07-17 16:32:40 -0500

    added card drawing

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Card from './components/Card';
 import {useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { io } from 'socket.io-client'
 
 const socket = io ('http://localhost:5000')
@@ -11,10 +11,12 @@ const socket = io ('http://localhost:5000')
 export default function App() {
   const [message, changeMessage] = useState('')
   const [deck, changeDeck] = useState('')
+  const [hand, updateHand] = useState([])
+
 
   socket.on('reset', function reset (res) {
-
     changeDeck(res)
+
     console.log(deck)
   })
   socket.on('response', function respond (res) {
@@ -24,18 +26,42 @@ export default function App() {
   socket.on('card-res', function card (res) {
     console.log(res)
   })
+
+  function handleDraw () {
+    if (deck.length === 0) {
+      return
+    }
+    var hand_copy = hand
+    var deck_copy = deck
+    const card = deck_copy.slice(-1)
+    const element = <li key = {card[1] + card[0]}>
+                      <Card 
+                      color = {card[0]} 
+                      number = {card[1]} 
+                      socket = {socket}
+                      />
+                    </li>
+    deck_copy = deck_copy.slice(0, -1)
+    hand_copy.push(element)
+    updateHand(hand_copy)
+    changeDeck(deck_copy)
+  }
   
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <button onClick = {() => {socket.emit('push')}}>
+      <Button onPress = {() => {socket.emit('push')}}>
         Push Me!
-      </button>
+      </Button>
       <Card color = 'red' number = '9' socket = {socket}></Card>
-      <button onClick = {() => {socket.emit('reset')}}>
-        test reset
-      </button>
-      {message}
+      <Button onPress = {() => {socket.emit('reset')}} title='test reset'/>
+      <Button 
+      onPress = {() => {handleDraw()}}
+      title = 'draw'
+      />
+      <ul>
+        {hand}
+      </ul>
       <StatusBar style="auto" />
     </View>
   );
--- components/Card.js
+++ components/Card.js
@@ -1,20 +1,20 @@
-import { StyleSheet } from "react-native"
+import {StyleSheet, Button} from "react-native"
 
 export default function Card (props) {
     return (
-        <div>
-            <button onClick = {() => {props.socket.emit('card', props.number)}}>
-                {props.number}<br/>
-                {props.color}<br/>
-            </button>
-        </div>
+            <Button
+            color = '#fff' 
+            onClick = {() => {props.socket.emit('card', props.number)}}
+            style = {styles.container}
+            title = {props.color + ' ' + props.number}
+            />
     )
 }
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      color: '#fff',
       borderStyle: 'solid',
       alignItems: 'center',
       justifyContent: 'center',
